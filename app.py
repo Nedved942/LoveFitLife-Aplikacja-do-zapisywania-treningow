2c9fb9bbb31addba9745d2850c6a29d4f60c08bb
nedved942 <pawelkoziol77@wp.pl>
2023-12-03 03:15:53 +0100

    Zdefiniowanie tabel w bazie danych: BodyParts, Exercises

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
 
+class BodyParts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body_parts = db.Column(db.String, nullable=False, unique=True)
+
+
+class Exercises(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    abbreviation = db.Column(db.String(10), nullable=False, unique=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    main_body_part = db.Column(db.String(120), nullable=False)
+    another_body_parts = db.Column(db.Text)
+    description = db.Column(db.Text)
+    media = db.Column(db.LargeBinary)
+    link = db.Column(db.String)
+
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     pass

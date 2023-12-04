4f02e8ba499c89f2e96368414fbccad25b20deca
nedved942 <pawelkoziol77@wp.pl>
2023-12-04 22:19:28 +0100

    Wyciagniecie definicji tabel bazy danych do pliku models.py

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from json import loads
+from json import loads, JSONDecodeError
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
+from models import BodyParts, Exercises
 
-class BodyParts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body_part = db.Column(db.String, nullable=False, unique=True)
-    abbreviation_character = db.Column(db.String(10), nullable=False, unique=True)
-
-
-class Exercises(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    abbreviation = db.Column(db.String(10), nullable=False, unique=True)
-    name = db.Column(db.String(120), nullable=False, unique=True)
-    name_ang = db.Column(db.String(120), unique=True)
-    main_body_part = db.Column(db.String(120), nullable=False)
-    another_body_parts = db.Column(db.Text)
-    description = db.Column(db.Text)
-    media = db.Column(db.LargeBinary)
-    link = db.Column(db.String)
+db.init_app(app)
 
 
 with app.app_context():
@@ -49,11 +35,15 @@ def exercise_database_view():
     for body_part, exercises in sorted_exercises.items():
         if exercises:
             for exercise in exercises:
-                list_of_another_body_parts = loads(exercise.another_body_parts)
-                exercise.another_body_parts = list_of_another_body_parts
+                if exercise.another_body_parts:
+                    try:
+                        list_of_another_body_parts = loads(exercise.another_body_parts)
+                    except JSONDecodeError:
+                        list_of_another_body_parts = [exercise.another_body_parts]
+                    exercise.another_body_parts = list_of_another_body_parts
 
-    app.logger.info(sorted_exercises)
-    app.logger.info(body_parts)
+    # app.logger.info(sorted_exercises)
+    # app.logger.info(body_parts)
     return render_template("exercise_database.html", sorted_exercises=sorted_exercises)
 
 
---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+
+class BodyParts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body_part = db.Column(db.String, nullable=False, unique=True)
+    abbreviation_character = db.Column(db.String(10), nullable=False, unique=True)
+
+
+class Exercises(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    abbreviation = db.Column(db.String(10), nullable=False, unique=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    name_ang = db.Column(db.String(120), unique=True)
+    main_body_part = db.Column(db.String(120), nullable=False)
+    another_body_parts = db.Column(db.Text)
+    description = db.Column(db.Text)
+    media = db.Column(db.LargeBinary)
+    link = db.Column(db.String)

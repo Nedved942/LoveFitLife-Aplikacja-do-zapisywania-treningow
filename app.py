fd52e56c5a12334494969276f1e3bc32a2afb5d0
nedved942 <pawelkoziol77@wp.pl>
2023-12-04 01:08:37 +0100

    Zmiana definicji tabel baz danych i dodanie widoku wyÅswietlania wszystkich cwiczen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from json import loads
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Secret_key"
@@ -10,13 +11,15 @@ db = SQLAlchemy(app)
 
 class BodyParts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body_parts = db.Column(db.String, nullable=False, unique=True)
+    body_part = db.Column(db.String, nullable=False, unique=True)
+    abbreviation_character = db.Column(db.String(10), nullable=False, unique=True)
 
 
 class Exercises(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     abbreviation = db.Column(db.String(10), nullable=False, unique=True)
     name = db.Column(db.String(120), nullable=False, unique=True)
+    name_ang = db.Column(db.String(120), unique=True)
     main_body_part = db.Column(db.String(120), nullable=False)
     another_body_parts = db.Column(db.Text)
     description = db.Column(db.Text)
@@ -35,9 +38,23 @@ def index():
 
 
 @app.route("/baza-cwiczen", methods=["GET", "POST"])
-def exercise_database():
-    pass
-    return render_template("exercise_database.html")
+def exercise_database_view():
+    # Pobranie danych z tabel i przypisanie ich posortowanych wg partii mięśniowych do słownika
+    sorted_exercises = {}
+    body_parts = BodyParts.query.with_entities(BodyParts.body_part).order_by(BodyParts.id).all()
+    for body_part in body_parts:
+        sorted_exercises[body_part[0]] = (Exercises.query.filter(Exercises.main_body_part == body_part[0]).all())
+
+    # Konwersja another_body_parts w słowniku sorted_exercises z typu string na typ list
+    for body_part, exercises in sorted_exercises.items():
+        if exercises:
+            for exercise in exercises:
+                list_of_another_body_parts = loads(exercise.another_body_parts)
+                exercise.another_body_parts = list_of_another_body_parts
+
+    app.logger.info(sorted_exercises)
+    app.logger.info(body_parts)
+    return render_template("exercise_database.html", sorted_exercises=sorted_exercises)
 
 
 @app.route("/dodawanie-treningu", methods=["GET", "POST"])

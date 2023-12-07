908e1d4eecca86995029c905992f45946d760f9c
nedved942 <pawelkoziol77@wp.pl>
2023-12-07 21:46:17 +0100

    Zmiana wwartości tabeli bazy danych w kolumnie another_body_parts z listy na pojedynczy string

--- app.py
+++ app.py
@@ -31,26 +31,15 @@ def exercise_database_view():
     for body_part in body_parts:
         sorted_exercises[body_part[0]] = (Exercises.query.filter(Exercises.main_body_part == body_part[0]).all())
 
-    # Konwersja another_body_parts w słowniku sorted_exercises z typu string na typ list
-    for body_part, exercises in sorted_exercises.items():
-        if exercises:
-            for exercise in exercises:
-                if exercise.another_body_parts:
-                    try:
-                        list_of_another_body_parts = loads(exercise.another_body_parts)
-                    except JSONDecodeError:
-                        list_of_another_body_parts = [exercise.another_body_parts]
-                    exercise.another_body_parts = list_of_another_body_parts
-
-    # app.logger.info(sorted_exercises)
     # app.logger.info(body_parts)
     return render_template("exercise_database.html", sorted_exercises=sorted_exercises)
 
 
 @app.route("/dodawanie-cwiczenia", methods=["GET", "POST"])
 def add_exercise():
-    pass
-    return render_template("add_exercise.html")
+    body_parts = BodyParts.query.with_entities(BodyParts.body_part).order_by(BodyParts.id).all()
+
+    return render_template("add_exercise.html", body_parts=body_parts)
 
 
 @app.route("/dodawanie-treningu", methods=["GET", "POST"])

4cff7c0cd4f910e02e7f0375e9b79be9467628b0
nedved942 <pawelkoziol77@wp.pl>
2023-12-15 16:17:59 +0100

    Dodanie kodu inicjujacego utworzenie tabel

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ migrate.init_app(app, db)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+with app.app_context():
+    db.create_all()
+
 
 @login_manager.user_loader
 def load_user(user_id):
